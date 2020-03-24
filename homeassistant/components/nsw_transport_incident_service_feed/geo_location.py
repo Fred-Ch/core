14c57643e1c23880cbb33337501d15d8742be621
Fred-Ch <33847462+Fred-Ch@users.noreply.github.com>
2020-03-24 11:20:43 +1100

    update syntax

--- homeassistant/components/nsw_transport_incident_service_feed/geo_location.py
+++ homeassistant/components/nsw_transport_incident_service_feed/geo_location.py
@@ -1,16 +1,15 @@
 """Support for NSW Transport Feeds."""
 from datetime import timedelta
 import logging
-from typing import Optional
 import re
+from typing import Optional
 
 from aio_geojson_nsw_transport_incidents import NswTransportServiceIncidentsFeedManager
 import voluptuous as vol
 
 from homeassistant.components.geo_location import PLATFORM_SCHEMA, GeolocationEvent
-from homeassistant.const import (
+from homeassistant.const import (  # ATTR_LOCATION,
     ATTR_ATTRIBUTION,
-    ATTR_LOCATION,
     CONF_LATITUDE,
     CONF_LONGITUDE,
     CONF_RADIUS,
@@ -29,24 +28,12 @@ from homeassistant.helpers.typing import ConfigType, HomeAssistantType
 
 _LOGGER = logging.getLogger(__name__)
 
-
-# ATTR_CATEGORY = "category"
-# ATTR_COUNCIL_AREA = "council_area"
-# ATTR_EXTERNAL_ID = "external_id"
-# ATTR_FIRE = "fire"
-# ATTR_PUBLICATION_DATE = "publication_date"
-# ATTR_RESPONSIBLE_AGENCY = "responsible_agency"
-# ATTR_SIZE = "size"
-# ATTR_STATUS = "status"
-# ATTR_TYPE = "type"
-
-ATTR_ATTRIBUTION = "attribution"
+# ATTR_ATTRIBUTION = "attribution"
 ATTR_TITLE = "title"
 ATTR_CATEGORY = "category"
 ATTR_EXTERNAL_ID = "external_id"
 ATTR_PUBLICATION_DATE = "publication_date"
 ATTR_DESCRIPTION = "description"
-# ATTR_OTHERADVICE = "otherAdvice"
 ATTR_PUBLICTRANSPORT = "publicTransport"
 ATTR_ADVICEA = "adviceA"
 ATTR_ADVICEB = "adviceB"
@@ -59,16 +46,17 @@ ATTR_DIVERSIONS = "diversions"
 ATTR_TYPE = "type"
 ATTR_SUBCATEGORY = "subCategory"
 ATTR_DURATION = "duration"
-ATTR_FEATURE_TYPE = "Feature_Type"
-ATTR_ROAD = "Road"
-ATTR_COUNCIL = "Council"
+ATTR_FEATURE_TYPE = "featureType"
+ATTR_ROAD = "road"
+ATTR_COUNCIL = "council"
+
+ATTR_PICTURE = "entity_picture"
 
 
 CONF_CATEGORIES = "categories"
 CONF_HAZARDS = "hazards"
 CONF_HAZARDS_STATE = "hazardsState"
 
-# CONF_CATEGORIES = "categories"
 
 DEFAULT_RADIUS_IN_KM = 20.0
 DEFAULT_UNIT_OF_MEASUREMENT = "km"
@@ -80,8 +68,8 @@ SIGNAL_UPDATE_ENTITY = "nsw_transport_incident_service_feed_update_{}"
 
 SOURCE = "nsw_transport_incident_service_feed"
 
-VALID_HAZARDS = ["Alpine", "Fire", "Flood", "Incident", "Major events", "Roadworks"]
-VALID_HAZARDS_STATE = ["Open","Closed",""]
+VALID_HAZARDS = ["alpine", "fire", "flood", "incident", "majorevent", "roadwork"]
+VALID_HAZARDS_STATE = ["open", "closed", "", None, "none", "None"]
 DEFAULT_HAZARDS = "Incident"
 DEFAULT_HAZARDS_STATE = "Open"
 
@@ -93,10 +81,13 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(
         # vol.Optional(CONF_HAZARDS_STATE, default=DEFAULT_HAZARDS_STATE): vol.All(
         #     vol.Coerce(str), vol.In(VALID_HAZARDS_STATE)
         # ),
-        
-        vol.Optional(CONF_CATEGORIES, default=[]):  cv.ensure_list,
-        vol.Optional(CONF_HAZARDS, default=DEFAULT_HAZARDS):  vol.Coerce(str),
-        vol.Optional(CONF_HAZARDS_STATE, default=DEFAULT_HAZARDS_STATE): vol.Coerce(str),
+        vol.Optional(CONF_CATEGORIES, default=[]): cv.ensure_list,
+        vol.Optional(CONF_HAZARDS, default=DEFAULT_HAZARDS): vol.All(
+            vol.Coerce(str), vol.In(VALID_HAZARDS)
+        ),
+        vol.Optional(CONF_HAZARDS_STATE, default=DEFAULT_HAZARDS_STATE): vol.All(
+            vol.Coerce(str), vol.In(VALID_HAZARDS_STATE)
+        ),
         vol.Optional(CONF_LATITUDE): cv.latitude,
         vol.Optional(CONF_LONGITUDE): cv.longitude,
         vol.Optional(CONF_RADIUS, default=DEFAULT_RADIUS_IN_KM): vol.Coerce(float),
@@ -115,13 +106,19 @@ async def async_setup_platform(
     )
     radius_in_km = config[CONF_RADIUS]
     categories = config.get(CONF_CATEGORIES)
-    if(config.get(CONF_HAZARDS_STATE).lower() == "none"):
+    if config.get(CONF_HAZARDS_STATE).lower() == "none":
         hazard = f"{config.get(CONF_HAZARDS).lower()}"
     else:
         hazard = f"{config.get(CONF_HAZARDS).lower()}-{config.get(CONF_HAZARDS_STATE).lower()}"
     # Initialize the entity manager.
     manager = NswTransportServiceFeedEntityManager(
-        hass, async_add_entities, scan_interval, coordinates, radius_in_km, categories, hazard
+        hass,
+        async_add_entities,
+        scan_interval,
+        coordinates,
+        radius_in_km,
+        categories,
+        hazard,
     )
 
     async def start_feed_manager(event):
@@ -218,8 +215,8 @@ class NswTransportServiceLocationEvent(GeolocationEvent):
     def __init__(self, feed_manager, external_id):
         """Initialize entity with data from feed entry."""
         self._feed_manager = feed_manager
-        self._external_id = external_id   
-        self._name = None     
+        self._external_id = external_id
+        self._name = None
         self._distance = None
         self._latitude = None
         self._longitude = None
@@ -244,6 +241,7 @@ class NswTransportServiceLocationEvent(GeolocationEvent):
         self._road = None
         self._council_area = None
         self._hazard = feed_manager._hazard
+        self._picture = None
 
     async def async_added_to_hass(self):
         """Call when entity is added to hass."""
@@ -293,28 +291,28 @@ class NswTransportServiceLocationEvent(GeolocationEvent):
         self._external_id = feed_entry.external_id
         self._publication_date = feed_entry.publication_date
         self._description = feed_entry.description
-        self._otherAdvice = re.sub('<[^<]+?>', '', feed_entry.otherAdvice)
+        self._otherAdvice = re.sub("<[^<]+?>", "", feed_entry.otherAdvice)
         self._publicTransport = feed_entry.publicTransport
         self._adviceA = feed_entry.adviceA
         self._adviceB = feed_entry.adviceB
-        self._adviceOther = re.sub('<[^<]+?>', '', feed_entry.adviceOther)
+        self._adviceOther = re.sub("<[^<]+?>", "", feed_entry.adviceOther)
         self._isMajor = feed_entry.isMajor
         self._isEnded = feed_entry.isEnded
         self._isNew = feed_entry.isNew
         self._isImpactNetwork = feed_entry.isImpactNetwork
-        self._diversions = re.sub('<[^<]+?>', '', feed_entry.diversions)
+        self._diversions = re.sub("<[^<]+?>", "", feed_entry.diversions)
         self._type = feed_entry.type
         self._subCategory = feed_entry.subCategory
         self._duration = feed_entry.duration
         self._feature_type = feed_entry.feature_type
         self._road = feed_entry.road
         self._council_area = feed_entry.council_area
-        
+        self._picture = self.picture
 
         self._distance = feed_entry.distance_to_home
         self._latitude = feed_entry.coordinates[0]
         self._longitude = feed_entry.coordinates[1]
-        
+
         # self._category = feed_entry.category
         # self._publication_date = feed_entry.publication_date
         # self._location = feed_entry.location
@@ -348,6 +346,35 @@ class NswTransportServiceLocationEvent(GeolocationEvent):
         if self._hazard.lower().startswith("roadwork"):
             return "mdi:road-variant"
         return "mdi:alarm-light"
+
+    @property
+    def picture(self):
+        """Return the picture to use in the frontend."""
+        if self._hazard.lower().startswith("incident"):
+            return (
+                "https://www.livetraffic.com/images/icons/hazard/traffic-incident.gif"
+            )
+            # return "https://img.icons8.com/color/high-risk"
+        if self._hazard.lower().startswith("fire"):
+            return (
+                "https://www.livetraffic.com/images/icons/hazard/weather-bush-fire.gif"
+            )
+            # return "https://img.icons8.com/color/fire-hazard"
+        if self._hazard.lower().startswith("flood"):
+            return "https://www.livetraffic.com/images/icons/hazard/weather-flood.gif"
+            # return "https://img.icons8.com/color/floods"
+        if self._hazard.lower().startswith("alpine"):
+            return (
+                "https://www.livetraffic.com/images/icons/hazard/weather-snow-ice.gif"
+            )
+            # return "https://img.icons8.com/color/snowflake"
+        if self._hazard.lower().startswith("major"):
+            return "https://www.livetraffic.com/images/icons/hazard/major-event.gif"
+            # return "https://img.icons8.com/color/star"
+        if self._hazard.lower().startswith("roadwork"):
+            return "https://www.livetraffic.com/images/icons/hazard/road-work-3.png"
+            # return "https://img.icons8.com/color/under-construction"
+        return None
 
     @property
     def source(self) -> str:
@@ -384,33 +411,33 @@ class NswTransportServiceLocationEvent(GeolocationEvent):
         """Return the device state attributes."""
         attributes = {}
         for key, value in (
-            (ATTR_TITLE , self._title),
-            (ATTR_FEATURE_TYPE , self._hazard),
-            (ATTR_TYPE , self._type),
-            (ATTR_SUBCATEGORY , self._subCategory),
-            (ATTR_ROAD , self._road ),
-            (ATTR_COUNCIL , self._council_area ),
-            (ATTR_CATEGORY , self._category),
-            (ATTR_DESCRIPTION , self._description),
-            (ATTR_PUBLICATION_DATE , self._publication_date),
-            (ATTR_ADVICEA , self._adviceA),
-            (ATTR_ADVICEB , self._adviceB),
-            (ATTR_DIVERSIONS , self._diversions),
-            (ATTR_PUBLICTRANSPORT , self._publicTransport),
-            (ATTR_ADVICEOTHER , self._adviceOther),
-            (ATTR_ISMAJOR , self._isMajor),
-            (ATTR_ISENDED , self._isEnded),
-            (ATTR_ISNEW , self._isNew),
-            (ATTR_ISIMPACTNETWORK , self._isImpactNetwork),
-            (ATTR_DURATION , self._duration),
-            (ATTR_EXTERNAL_ID , self._external_id),
-            (ATTR_ATTRIBUTION , self._attribution),
-
+            (ATTR_TITLE, self._title),
+            (ATTR_FEATURE_TYPE, self._hazard),
+            (ATTR_TYPE, self._type),
+            (ATTR_SUBCATEGORY, self._subCategory),
+            (ATTR_ROAD, self._road),
+            (ATTR_COUNCIL, self._council_area),
+            (ATTR_CATEGORY, self._category),
+            (ATTR_DESCRIPTION, self._description),
+            (ATTR_PUBLICATION_DATE, self._publication_date),
+            (ATTR_ADVICEA, self._adviceA),
+            (ATTR_ADVICEB, self._adviceB),
+            (ATTR_DIVERSIONS, self._diversions),
+            (ATTR_PUBLICTRANSPORT, self._publicTransport),
+            (ATTR_ADVICEOTHER, self._adviceOther),
+            (ATTR_ISMAJOR, self._isMajor),
+            (ATTR_ISENDED, self._isEnded),
+            (ATTR_ISNEW, self._isNew),
+            (ATTR_ISIMPACTNETWORK, self._isImpactNetwork),
+            (ATTR_DURATION, self._duration),
+            (ATTR_EXTERNAL_ID, self._external_id),
+            (ATTR_ATTRIBUTION, self._attribution),
+            (ATTR_PICTURE, self._picture),
         ):
             if value or isinstance(value, bool):
                 attributes[key] = value
         return attributes
-    
-    def cleanuphtml(self, prop):
-        return re.sub('<[^<]+?>', '', prop)
 
+    def cleanuphtml(self, prop):
+        """Clean string from HTML tags."""
+        return re.sub("<[^<]+?>", "", prop)
